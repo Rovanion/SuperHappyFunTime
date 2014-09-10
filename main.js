def061b681a86102dab92ea2f0491efe1ce8d3f1
Samuel Nilsson <samni698@gmail.com>
2014-09-10 13:45:28 +0200

    Has added a sprite sheet of the main character. main.js adds the spritesheet as an asset and renders it at the left bottom of the screen. Also the phaser logo has been removed.

--- main.js
+++ main.js
@@ -3,12 +3,11 @@ window.onload = function() {
         var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
 
         function preload () {
-		game.load.image('logo', 'phaser.png');
+		game.load.spritesheet('character', 'assets/character_sprite_sheet.png', 64, 79);
         }
 
         function create () {
-		var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-		logo.anchor.setTo(-1, 0);
+		var character = game.add.sprite(30, game.world.height - 120, 'character');
         }
 
 	function update(){
