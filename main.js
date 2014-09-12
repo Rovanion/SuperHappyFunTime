a7ef1b13ca4af58e9b118c8548b0190562c73e4d
Samuel Nilsson <samni698@gmail.com>
2014-09-12 15:30:37 +0200

    Has now you can use the right cursor to move right and the up cursor to jump.

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ window.onload = function() {
 
 	var platforms;
 	var character;
+	var cursors;
 
         function create () {
 		game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -35,9 +36,24 @@ window.onload = function() {
 		character.body.collideWorldBounds = true;
 
 		character.animations.add('right', [0, 1, 2, 3], 10);
+
+		cursors = game.input.keyboard.createCursorKeys();
         }
 
 	function update() {
 		game.physics.arcade.collide(character, platforms);
+
+		if (cursors.right.isDown) {
+			character.body.velocity.x = 300;
+			
+			character.animations.play('right');
+		}
+		else {
+			character.animations.stop();
+		}
+
+		if (cursors.up.isDown && character.body.touching.down) {
+			character.body.velocity.y = -200;
+		}
 	}
 };
