41bd1bd20af203772234f23dd3dcc3e8366b6837
Samuel Nilsson <samni698@gmail.com>
2014-09-29 16:14:33 +0200

    Added a tutoring text.

--- Level1.js
+++ Level1.js
@@ -6,4 +6,5 @@ Level1.prototype.loadLevelObjects = function() {
 
 	this.addPlatform(0, this.world.height - 64, 800, 64);
 
+	this.addText(100, this.world.height - 180, "Press left and right arrows to move");
 };
