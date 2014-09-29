3f1315fea71f32c0f0129b1aebd0134791ccad64
Rovanion Luckey <rovanion.luckey@gmail.com>
2014-09-29 16:03:46 +0200

    A working hookshot prototype implemented.

--- HookShot.js
+++ HookShot.js
@@ -1,5 +1,6 @@
-HookShot = function(gameplaystate) {
-	this.gameplaystate = gameplaystate;
+HookShot = function(gameState, parent) {
+	this.gameState = gameState;
+	this.parent = parent;
 };
 
 HookShot.prototype = {
@@ -7,25 +8,36 @@ HookShot.prototype = {
 	 * Function which should be called before the class is used in order to load it's assets
 	 */
 	preload: function() {
-		game.load.image('chain', 'assets/chain.jpg', 64, 79);
-		this.sprite = this.gameplaystate.add.sprite(50, 50, 'chain');
-		this.gameplaystate.physics.arcade.enable(this.sprite);
+		this.gameState.load.image('chain', 'assets/chain.png', 64, 79);
 	},
 
 	create: function() {
-		this.sprite = game.add.sprite(200, 20, 'chain');
+		this.sprite = this.gameState.add.sprite(-100, -100, 'chain');
+		this.gameState.physics.arcade.enable(this.sprite);
+	},
+
+	update: function(){
+		this.gameState.physics.arcade.overlap(this.sprite, this.gameState.platforms, this.hit, null, this);
+
 	},
 
 	/**
 	 * Shoot the hookshot from one position to the mouse.
 	 * Arguments are:
 	 * x: The x-coordinate of the target.
-	 * y:  The y-coordinate of the target.
+	 * y: The y-coordinate of the target.
 	 * attache: The object which should be moved to the target.
-	 * Returns the target which was hit.
 	 */
-	shoot: function(x, y, attache) {
-		game.physics.arcade.moveToPointer(attache, 500);
-		return null;
+	shoot: function() {
+		this.sprite.reset(this.parent.sprite.x, this.parent.sprite.y);
+		this.sprite.rotation = game.physics.arcade.moveToPointer(this.sprite, 1000, game.input.activePointer, 500);
+	},
+
+	/**
+	 * Called once the hook hits something.
+	 */
+	hit: function() {
+		// Go after the hookshot
+		game.physics.arcade.moveToObject(this.parent.sprite, this.sprite, 1000);
 	}
 };
