ba8dd153200d9ff84c43f6bfdfc5c40d4ebfd2d6
Rovanion Luckey <rovanion.luckey@gmail.com>
2014-09-29 14:06:26 +0200

    main.js - Removed unnecicery lines of code.

--- main.js
+++ main.js
@@ -3,10 +3,8 @@ window.onload = function() {
 	game = new Phaser.Game(800, 200, Phaser.AUTO, '', {create: create});
 
 	function create () {
-		state = new Level1();
-		game.state.add('level1', state);
-		game.state.start("level1");
 		setupLevels();
+		game.state.start('1');
 	}
 
 	/**
