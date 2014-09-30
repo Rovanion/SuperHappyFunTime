126bb5d83f15c40695d8a52086b9316567209286
Samuel Nilsson <samni698@gmail.com>
2014-09-30 09:26:36 +0200

    Solves the problem regarding the tutoring doesn't reload properly when the level reloads.

--- Level1.js
+++ Level1.js
@@ -1,6 +1,5 @@
 Level1 = function() {
-	this.leftRightPressed = false;
-	this.upPressed = false;
+	
 };
 
 Level1.prototype = new Level(1600, 600);
@@ -10,6 +9,10 @@ Level1.prototype.loadLevelObjects = function() {
 	this.addPlatform(0, this.world.height - 64, 800, 64);
 
 	this.addText(100, this.world.height - 180, "Press left and right arrows to move");
+
+	this.leftRightPressed = false;
+	
+	this.upPressed = false;
 
 };
 
