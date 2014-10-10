1d8b4902ec669e5ad594bbcaf917eafa595a35a0
Rovanion Luckey <rovanion.luckey@gmail.com>
2014-10-10 21:43:54 +0200

    Fixed the timer for good this time.

--- Timer.js
+++ Timer.js
@@ -12,23 +12,27 @@ Timer.prototype = {
 	create: function(){
 		this.background = this.gameState.add.sprite(0, game.height - 60, 'time-background');
 		this.background.scale.x = this.background.scale.y = 0.4;
-		this.counterText = this.gameState.add.text(37, game.height - 49, '0.00', {
+		this.counterText = this.gameState.add.text(45, game.height - 49, '0.00', {
 			font: "26px Verdana",
 			fill: "#fff",
 			align: "center"
 		});
 		// The texts position is relative to the cameras.
 		this.counterText.fixedToCamera = this.background.fixedToCamera = true;
-		this.gameState.time.events.loop(10, this.updateCounter, this);
+		this.gameState.time.events.loop(100, this.updateCounter, this);
 
 	},
 
 	updateCounter: function() {
 		this.counter++;
-		this.counterText.setText(this.counter / 100);
-		if(this.counter < 1000)
-			this.counterText.setText(this.counter / 100);
+		if(this.counter % 10 === 0){
+			this.counterText.setText(this.counter / 10 + ".0");
+			if(this.counter === 100)
+				this.counterText.cameraOffset.x = 38;
+			else if(this.counter === 1000)
+				this.counterText.cameraOffset.x = 30;
+		}
 		else
-			this.counterText.setText((this.counter / 100).toFixed(1));
+			this.counterText.setText(this.counter / 10);
 	}
 }
