92e252b576af5a53053e487e89108591d5e3613b
Samuel Nilsson <samni698@gmail.com>
2014-10-10 17:47:11 +0200

    Now we use .csv tilemap files to easier and faster create levels. It can be helpful to generate these files using the toast editor at http://toastedware.com/toasteditor/. The commit includes an example level.

--- Level.js
+++ Level.js
@@ -1,17 +1,16 @@
-Level = function(levelWidth, levelHeight) {
-	this.width = levelWidth;
-	this.height = levelHeight;
+Level = function (csvfile, characterStartX, characterStartY) {
+	this.csvfile = csvfile;
+	this.characterStartX = characterStartX;
+	this.characterStartY = characterStartY;
 };
 
 Level.prototype = {
-	characterStartX: 60,
-	characterStartY: 150,
-	platforms: null,
-	counter: 0,
-	counterText: null,
 
 	preload: function() {
-		this.loadAssets();
+		this.load.image('background', 'assets/background.png');
+
+		this.load.tilemap('map', this.csvfile, null, Phaser.Tilemap.CSV);
+		this.load.image('tilemap', 'assets/platformblock.png');
 
 		this.bobby = new Character(this);
 		bobby = this.bobby;
@@ -22,63 +21,37 @@ Level.prototype = {
 	},
 
 	create: function() {
-		this.physics.startSystem(Phaser.Physics.Arcade);
-		this.add.tileSprite(0, 0, this.width, this.height, 'background');
-		this.world.setBounds(0, 0, this.width, this.height);
-		this.platforms = this.add.group();
-		this.platforms.enableBody = true;
+		var map = game.add.tilemap('map', 40, 40);
 
-		this.loadLevelObjects();
+		map.addTilesetImage('tilemap');
+
+		this.physics.startSystem(Phaser.Physics.Arcade);
+
+		// Sets the size of the world depending on the size of the map
+		this.add.tileSprite(0, 0, map.widthInPixels, map.heightInPixels, 'background');
+		this.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+		this.layer = map.createLayer(0);
+
+		// Objects can collide with the tile
+		map.setCollisionBetween(0, 0);
 
 		this.bobby.create(this.characterStartX, this.characterStartY);
 		this.timer.create();
 
 		// Register hooks for the number keys to switch between levels.
 		keynames = [ "", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX",
-								 "SEVEN", "EIGHT", "NINE", "TEN", "ZERO" ];
+		"SEVEN", "EIGHT", "NINE", "TEN", "ZERO" ];
 		for ( var n = 1; n <= 10; n++){
-			// The extra anonymous function here exists because of:
-			// http://www.mennovanslooten.nl/blog/post/62
-			switchLevel = ( function(n) { return function () { game.state.start(n); } } )(n);
-			// Equivelant of this.input.keyboard.addKey(49,50, .. ,58)
-			this.input.keyboard.addKey(Phaser.Keyboard[keynames[n]]).onDown.add(switchLevel);
-		}
-	},
-
-	update: function() {
-		this.bobby.update();
-
-	},
-
-	render: function() {
-		this.bobby.render();
-	},
-
-	loadAssets: function() {
-		this.load.image('background', 'assets/background.png');
-		this.load.image('platform', 'assets/platform.png');
-	},
-
-	/**
-	 * Adds a platform to the level.
-	 * Arguments, all in pixels:
-	 * x: The horizontal coordinate from the left edge.
-	 * y: The vertical coordinate from the top edge.
-	 * width: The horizontal width of the platform.
-	 * height: The vertical height of the platform.
-	 */
-	addPlatform: function(x, y, width, height) {
-		var platform = this.platforms.create(x, y, 'platform');
-		platform.scale.setTo(width/400, height/32);
-		platform.body.immovable = true;
-	},
-
-	addText: function(positionx, positiony, text) {
-		var style = {font: "26px Verdana", fill: "ffffff", align: "center"};
-		this.tutoringText = this.add.text(positionx, positiony, text, style);
-	},
-
-	changeText: function(text) {
-		this.tutoringText.setText(text);
+		// The extra anonymous function here exists because of:
+		// http://www.mennovanslooten.nl/blog/post/62
+		switchLevel = ( function(n) { return function () { game.state.start(n); } } )(n);
+		// Equivelant of this.input.keyboard.addKey(49,50, .. ,58)
+		this.input.keyboard.addKey(Phaser.Keyboard[keynames[n]]).onDown.add(switchLevel);
 	}
+},
+
+update: function() {
+	this.bobby.update();
+}
 };
